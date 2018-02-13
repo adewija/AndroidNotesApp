7bdc7f21cd1113db0b793cf250ab4e729c50f904
Ade Wija Nugraha <adewijanugraha@Ade-Wija-Nugraha.local>
2018-02-13 08:50:59 +0700

    no message

---
+++ app/src/main/java/com/awn/app/mynotesapp/db/DatabaseContract.java
@@ -0,0 +1,20 @@
+package com.awn.app.mynotesapp.db;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by adewijanugraha on 13/02/18.
+ */
+
+public class DatabaseContract {
+
+    static String TABLE_NOTE = "note";
+    static final class NoteColumns implements BaseColumns {
+        //Note title
+        static String TITLE = "title";
+        //Note description
+        static String DESCRIPTION = "description";
+        //Note date
+        static String DATE = "date";
+    }
+}
